ba8b907da862902874bdb4b6ed9da6636ea7b659
SIDDHANT DIXIT <siddhant.dixit23@gmail.com>
2021-09-01 17:10:21 +0530

    Revert "Update index.js"
    This reverts commit 6892bf3a666713518f390141a6edc383ff6e30e5.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { context } = require('@actions/github/lib/utils');
 
 const run = async () => {
     // Get octokit
